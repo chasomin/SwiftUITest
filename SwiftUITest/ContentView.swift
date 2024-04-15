3a62c97f6cf6fd42a3b1e74c95d40c834062bd8f
chasomin <somin49@naver.com>
2024-04-15 15:46:32 +0900

    첫번째 UI 연습

--- SwiftUITest/ContentView.swift
+++ SwiftUITest/ContentView.swift
@@ -9,11 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("포인트를 더 모을 수 있게 맞춤 혜택을\n추천해드릴까요?")
+                .font(.title2.bold())
+                .padding(.init(top: 0, leading: 0, bottom: 30, trailing: 0))
+            
+            HStack {
+                Image(systemName: "calendar.badge.checkmark")
+                    .foregroundStyle(.blue)
+                    .font(.title2)
+                    .padding(.trailing)
+                Text("매일 포인트 받는 출석체크∙퀴즈")
+            }
+            .padding(.bottom)
+
+            HStack {
+                Image(systemName: "n.circle.fill")
+                    .foregroundStyle(.red)
+                    .font(.title2)
+                    .padding(.trailing)
+                Text("새로운 이벤트")
+            }
+            .padding(.bottom)
+
+            HStack {
+                Image(systemName: "star.fill")
+                    .foregroundStyle(.yellow)
+                    .font(.title2)
+                    .padding(.trailing)
+                Text("미션 추천")
+            }
+            
+            Spacer()
+            
+            Text("[선택] 맞춤형 서비스 이용 동의")
+                .padding(.bottom)
+            
+            VStack(alignment: .center) {
+                Button {
+                    
+                } label: {
+                    Text("동의하기")
+                        .foregroundStyle(.white)
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.all)
+                .background(.blue, in: .buttonBorder)
+                
+                Button {
+                    
+                } label: {
+                    Text("다음에 하기")
+                        .foregroundStyle(.gray)
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.all)
+            }
         }
         .padding()
     }
@@ -21,4 +72,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
