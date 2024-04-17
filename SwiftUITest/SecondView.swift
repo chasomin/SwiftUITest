9857ac80947c3a62608bd9449071825a711acb78
chasomin <somin49@naver.com>
2024-04-17 19:01:55 +0900

    customView 메서드, struct로 분리 / customModifier 적용

--- SwiftUITest/SecondView.swift
+++ SwiftUITest/SecondView.swift
@@ -10,6 +10,19 @@ import SwiftUI
 struct SecondView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 15) {
+            Title()
+            
+            OXView()
+            
+            Spacer()
+            
+            TossView()
+        }
+        .padding()
+    }
+    
+    func Title() -> some View {
+        VStack(alignment: .leading, spacing: 15) {
             Image(systemName: "scribble.variable")
                 .font(.largeTitle)
                 .foregroundStyle(.pink)
@@ -20,91 +33,76 @@ struct SecondView: View {
             Text("매일 푸는 금융 퀴즈 290,300명 참여중")
                 .font(.callout)
                 .foregroundStyle(.gray)
-            
-            HStack {
-                Button(action: {}, label: {
-                    VStack {
-                        Image(systemName: "poweroff")
-                            .font(.largeTitle.bold())
-                            .padding(.bottom, 6)
-                        Text("그렇다")
-                    }
-                })
-                .padding(.all)
-                .frame(maxWidth: .infinity)
-                .background(.blue.opacity(0.4), in: .buttonBorder)
-                
-                Button {
-                } label: {
-                    VStack {
-                        Image(systemName: "xmark")
-                            .font(.largeTitle.bold())
-                            .padding(.bottom, 10)
-                        Text("아니다")
-                    }
-                }
-                .padding()
-                .foregroundStyle(.red)
-                .frame(maxWidth: .infinity)
-                .background(.red.opacity(0.4), in: .buttonBorder)
-
-
-            }
-            .frame(maxWidth: .infinity)
-            .padding(.bottom)
-            
-            Spacer()
-            
-            HStack {
-                Button(action: {}, label: {
-                    VStack {
-                        Image(systemName: "b.square.fill")
-                            .foregroundStyle(.blue)
-                            .font(.title)
-                            .padding(.bottom, 5)
-                        Text("토스뱅크")
-                            .foregroundStyle(.white)
-                            .bold()
-                    }
-                })
-                .padding()
-                .frame(maxWidth: .infinity)
-                .background(.gray.opacity(0.4), in: .buttonBorder)
-                Button(action: {}, label: {
-                    VStack {
-                        Image(systemName: "chart.line.uptrend.xyaxis")
-                            .foregroundStyle(.red)
-                            .font(.title)
-                            .padding(.bottom, 5)
-                        Text("토스증권")
-                            .foregroundStyle(.white)
-                            .bold()
-                    }
-                })
-                .padding()
-                .frame(maxWidth: .infinity)
-                .background(.gray.opacity(0.4), in: .buttonBorder)
-                Button(action: {}, label: {
-                    VStack {
-                        Image(systemName: "headphones")
-                            .foregroundStyle(.blue)
-                            .font(.title)
-                            .padding(.bottom, 5)
-                        Text("고객센터")
-                            .foregroundStyle(.white)
-                            .bold()
-                    }
-                })
-                .padding()
-                .frame(maxWidth: .infinity)
-                .background(.gray.opacity(0.4), in: .buttonBorder)
-
-            }
         }
-        .padding()
     }
 }
 
 #Preview {
     SecondView()
+}
+
+struct OXView: View {
+    var body: some View {
+        HStack {
+            Button(action: {}, label: {
+                VStack {
+                    Image(systemName: "poweroff")
+                        .font(.largeTitle.bold())
+                        .padding(.bottom, 6)
+                    Text("그렇다")
+                }
+            })
+            .asRoundedRectangle(backColor: .blue, textColor: .blue)
+            
+            Button {
+            } label: {
+                VStack {
+                    Image(systemName: "xmark")
+                        .font(.largeTitle.bold())
+                        .padding(.bottom, 10)
+                    Text("아니다")
+                }
+            }
+            .asRoundedRectangle(backColor: .red, textColor: .red)
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.bottom)
+    }
+}
+
+struct TossView: View {
+    var body: some View {
+        HStack {
+            Button(action: {}, label: {
+                VStack {
+                    Image(systemName: "b.square.fill")
+                        .asTitleSizeImage(color: .blue)
+                        .padding(.bottom, 5)
+                    Text("토스뱅크")
+                        .bold()
+                }
+            })
+            .asRoundedRectangle(backColor: .gray, textColor: .white)
+            Button(action: {}, label: {
+                VStack {
+                    Image(systemName: "chart.line.uptrend.xyaxis")
+                        .asTitleSizeImage(color: .red)
+                        .padding(.bottom, 5)
+                    Text("토스증권")
+                        .bold()
+                }
+            })
+            .asRoundedRectangle(backColor: .gray, textColor: .white)
+            Button(action: {}, label: {
+                VStack {
+                    Image(systemName: "headphones")
+                        .asTitleSizeImage(color: .blue)
+                        .padding(.bottom, 5)
+                    Text("고객센터")
+                        .bold()
+                }
+            })
+            .asRoundedRectangle(backColor: .gray, textColor: .white)
+        }
+    }
 }
